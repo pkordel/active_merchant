5a511841daedd44b444f449c385923f24458bc30
Peter Kordel <pkordel@gmail.com>
2008-12-12 18:33:12 +0100

    Added return class stub, cleanup

--- lib/active_merchant/billing/integrations/dibs/notification.rb
+++ lib/active_merchant/billing/integrations/dibs/notification.rb
@@ -31,41 +31,8 @@ module ActiveMerchant #:nodoc:
             params['test'] == 'test'
           end
 
-          # Acknowledge the transaction to Dib. This method has to be called after a new 
-          # apc arrives. Dib will verify that all the information we received are correct and will return a 
-          # ok or a fail. 
-          # 
-          # Example:
-          # 
-          #   def ipn
-          #     notify = DibNotification.new(request.raw_post)
-          #
-          #     if notify.acknowledge 
-          #       ... process order ... if notify.complete?
-          #     else
-          #       ... log possible hacking attempt ...
-          #     end
           def acknowledge      
             true
-            # payload = raw
-            # 
-            # uri = URI.parse(Dib.notification_confirmation_url)
-            # 
-            # request = Net::HTTP::Post.new(uri.path)
-            # 
-            # request['Content-Length'] = "#{payload.size}"
-            # request['User-Agent'] = "Active Merchant -- http://home.leetsoft.com/am"
-            # request['Content-Type'] = "application/x-www-form-urlencoded" 
-            # 
-            # http = Net::HTTP.new(uri.host, uri.port)
-            # http.verify_mode    = OpenSSL::SSL::VERIFY_NONE unless @ssl_strict
-            # http.use_ssl        = true
-            # 
-            # response = http.request(request, payload)
-            # 
-            # # Replace with the appropriate codes
-            # raise StandardError.new("Faulty Dib result: #{response.body}") unless ["AUTHORISED", "DECLINED"].include?(response.body)
-            # response.body == "AUTHORISED"
           end
  private
 
